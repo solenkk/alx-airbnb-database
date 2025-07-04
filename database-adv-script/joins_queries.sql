2a74dd926a6042e10b85576cb68e0e07f206fc00
solen <kassahunsolen@gmail.com>
2025-07-05 00:47:48 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,20 @@
+--writing query using inner join 
+inner_join so when we use inner_join query what it basically does is joining the pairs so for our we have created about 
+about 9 tables in our air bnb project. 
+so when we match the tables it only matches the one that are in pair means IF we join table user and booking it will only
+join the users that made a booking with the booking table or if we join table of payment and user it will retirave datas of 
+the user and the payment they made that is what inner join is so 
+
+  -- the query looks like 
+  SELECT users.user_id, bookings.booking_id
+  FROM users
+  INNER JOIN users ON booking_id=users.booking_id;
+
+--the query for left join 
+SELECT users.user_id, bookings.booking_id
+FROM users
+LEFT JOIN users ON booking_id=users.booking_id;
+--THE query for full outer join 
+SELECT users.user_id, bookings.booking_id
+FROM users
+FULL OUTER JOIN  users ON booking_id=users.booking_id;
