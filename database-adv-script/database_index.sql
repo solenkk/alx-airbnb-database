35894fa3f6f5317b9f38b0830bea56a4ab72c53e
solen <kassahunsolen@gmail.com>
2025-07-06 02:29:36 +0300

    Create database_index.sql

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,17 @@
+-- Using WHERE
+SELECT user.email, property.property_id
+FROM users
+INNER JOIN property ON user.email = property.email
+WHERE property.property_id = '555BD';
+
+-- Using ORDER BY
+SELECT booking.total_price, payment.amount 
+FROM booking 
+LEFT JOIN payment ON booking.booking_id = payment.booking_id
+ORDER BY booking.total_price ASC;
+
+-- Creating an INDEX
+CREATE INDEX idx_users_email ON users(email);
+CREATE INDEX p_rp ON property(property_id);
+CREATE INDEX nk ON booking(total_price);
+CREATE INDEX pyt ON payment(amount);
